b854e43bd879b243e1f12bf0dc86de18b815e7f5
kenji-special <k.saito.1985@gmail.com>
2015-01-25 20:01:57 +0900

    added add and multiple function and updated test.

--- test/test.js
+++ test/test.js
@@ -15,3 +15,30 @@ test('copy', function(t){
     t.equal(vecCopy.elements[0], 1, 'these two numbers are equal');
     t.end();
 });
+
+test("subtract", function(t){
+    var vec1 = new Vector.Vector2([2, 2, 2]);
+    var vec2 = new Vector.Vector2([1, 1, 1]);
+    vec1.subtract(vec2);
+
+    t.equal(vec1.elements[0], 1, 'these two numbers are equal');
+    t.end();
+
+});
+
+test('multiple', function(t){
+    var vec = new Vector.Vector2([2, 2]);
+    vec.multiple(10);
+
+    t.equal(vec.elements[0], 20, 'these two numbers are equal');
+    t.end();
+});
+
+test('calculatrion', function(t){
+    var vec = new Vector.Vector2([2, 2]);
+    var vec1 = new Vector.Vector2([1, 1]);
+    vec.multiple(2).subtract(vec1);
+
+    t.equal(vec.elements[0], 3, 'these two numbers are equal');
+    t.end();
+});
